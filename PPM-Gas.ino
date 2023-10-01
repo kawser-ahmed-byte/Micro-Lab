0c9a9ee616e90da37a3ffd1f7f1c330a6adcf5f1
Kawser Ahmed <126769837+kawser-ahmed-byte@users.noreply.github.com>
2023-10-01 13:19:56 +0600

    Add files via upload

---
+++ PPM-Gas.ino
@@ -0,0 +1,33 @@
+#include<MQ2.h>
+
+
+const int sensorPin = A0;
+
+
+
+#define RL 10 
+#define m -0.263 
+#define b 0.42 
+#define Ro 20 
+void setup() {
+Serial.begin(9600);
+Serial.println("MQ2 warming up!");
+delay(20000); 
+pinMode(sensorPin, INPUT);
+
+
+}
+void loop() {
+float VRL; 
+float Rs; 
+float ratio; 
+float sensorValue = analogRead(sensorPin);
+Serial.println(sensorValue);
+VRL = sensorValue * (5.0/1023.0); 
+Rs = ((5.0*RL));
+ratio = Rs/Ro; 
+float ppm = pow(10, ((log10(ratio)-b)/m)); 
+Serial.print("PPM: ");
+delay(1000); 
+Serial.println(ppm);
+}
