28a7c1cc5cd6373324f1bbeceb3ad9ee0290f739
Kawser Ahmed <126769837+kawser-ahmed-byte@users.noreply.github.com>
2023-10-01 15:31:44 +0600

    Add files via upload

---
+++ Gas-Reading.ino
@@ -0,0 +1,18 @@
+int analogVal;
+float x;
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+
+}
+
+void loop() {
+  analogVal=analogRead(A0);
+   Serial.print("analog Value: ");
+  Serial.println(analogVal);
+  delay(1000);
+  Serial.print("Gas Value: ");
+   x= (analogVal*5.0)/1023;
+  Serial.println(x);
+  delay(1000);
+}
